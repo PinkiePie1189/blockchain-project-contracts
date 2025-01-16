155a607bdcadc2cf94c97255cb6b36e6d757760c
94794 <teodorsorescu@gmail.com>
2025-01-16 16:09:05 +0200

    Cleanup and added working test flow for blacksmith: get item -> upgrade -> burn

--- blacksmith/tests/blacksmith.ts
+++ blacksmith/tests/blacksmith.ts
@@ -5,90 +5,84 @@ import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
 import { MPL_CORE_PROGRAM_ID } from "@metaplex-foundation/mpl-core";
 
 describe("blacksmith", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
   const program = anchor.workspace.Blacksmith as Program<Blacksmith>;
 
-  let [pda] = PublicKey.findProgramAddressSync(
+  const [pda] = PublicKey.findProgramAddressSync(
     [Buffer.from("owner_pda")],
     program.programId
   );
 
-  // it("Create Asset", async () => {
+  const [pdaUser] = PublicKey.findProgramAddressSync(
+    [Buffer.from("user_pda")],
+    program.programId
+  );
 
-  //   let createAssetArgs = {
-  //     name: 'My Asset',
-  //     uri: 'https://example.com/my-asset.json',
-  //   };
-
-  //   let [pdaUser] = PublicKey.findProgramAddressSync(
-  //     [Buffer.from("user_pda")],
-  //     program.programId
-  //   );
-
-  //   console.log(pda, pdaUser);
-  
-  //   let asset = Keypair.generate();
-  //   const createAssetTx = await program.methods.requestItem(true)
-  //     .accountsPartial({
-  //       asset: asset.publicKey,
-  //       signer: anchor.Wallet.local().publicKey,
-  //       systemProgram: SystemProgram.programId,
-  //       coreProgram: MPL_CORE_PROGRAM_ID,
-  //       ownerPda: pda,
-  //       user: pdaUser
-  //     })
-  //     .signers([asset, anchor.Wallet.local().payer])
-  //     .rpc();
-  
-  //   console.log(createAssetTx);
-  // });
-
-  // it("Transfer NFT", async () => {
-  //   let asset = new PublicKey("2T4frzu4pic9siNWJ5aTDTUsmCXV8goS1yNFuxR73x6r")
-  //   const createAssetTx = await program.methods.transferNft()
-  //     .accountsPartial({
-  //       asset: asset,
-  //       signer: anchor.Wallet.local().publicKey,
-  //       newOwner: new PublicKey("AfYgWMf8enCy81Xni5sKBui2EddZSMuJjjTFgnkCmUpQ"),
-  //       coreProgram: MPL_CORE_PROGRAM_ID
-  //     })
-  //     .signers([anchor.Wallet.local().payer])
-  //     .rpc();
-  
-  //   console.log(createAssetTx);
-  // })
-
-  it("Upgrade NFT", async () => {
-    let asset = new PublicKey("d9gUf9YjGfhyLpwt7AZbtfbhR4fpx9347Wf7CP9sW9j")
-    const createAssetTx = await program.methods.upgradeNft()
+  const asset = Keypair.generate();
+  console.log(asset.publicKey)
+  it("Request Item", async () => {
+    const createAssetTx = await program.methods
+      .requestItem(true)
       .accountsPartial({
-        asset: asset,
-        payer: anchor.Wallet.local().publicKey,
-        authority: pda,
+        asset: asset.publicKey,
+        signer: anchor.Wallet.local().publicKey,
+        systemProgram: SystemProgram.programId,
         coreProgram: MPL_CORE_PROGRAM_ID,
-        systemProgram: SystemProgram.programId
+        ownerPda: pda,
+        user: pdaUser,
       })
-      .signers([anchor.Wallet.local().payer])
+      .signers([asset, anchor.Wallet.local().payer])
       .rpc();
-  
+
     console.log(createAssetTx);
-  })
+  });
 
-  it("SCRAP NFT", async() =>  {
-    let asset = new PublicKey("d9gUf9YjGfhyLpwt7AZbtfbhR4fpx9347Wf7CP9sW9j")
-    const scrap = await program.methods.scrapItem()
+  it("Upgrade Item", async () => {
+    const upgradeNftTx = await program.methods
+      .upgradeNft()
       .accountsPartial({
-        asset: asset,
+        asset: asset.publicKey,
         payer: anchor.Wallet.local().publicKey,
         authority: pda,
         coreProgram: MPL_CORE_PROGRAM_ID,
-        systemProgram: SystemProgram.programId
+        systemProgram: SystemProgram.programId,
       })
       .signers([anchor.Wallet.local().payer])
       .rpc();
-  
-    console.log(scrap);
-  })
+
+    console.log(upgradeNftTx);
+  });
+
+  it("Scrap NFT", async () => {
+    const scrapNftTx = await program.methods
+      .scrapItem()
+      .accountsPartial({
+        asset: asset.publicKey,
+        payer: anchor.Wallet.local().publicKey,
+        authority: pda,
+        coreProgram: MPL_CORE_PROGRAM_ID,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([anchor.Wallet.local().payer])
+      .rpc();
+
+    console.log(scrapNftTx);
+  });
+
+  /*
+  it("Transfer NFT", async () => {
+    const asset = new PublicKey("2T4frzu4pic9siNWJ5aTDTUsmCXV8goS1yNFuxR73x6r");
+    const transferNftTx = await program.methods.transferNft()
+      .accountsPartial({
+        asset: asset,
+        signer: anchor.Wallet.local().publicKey,
+        newOwner: new PublicKey("AfYgWMf8enCy81Xni5sKBui2EddZSMuJjjTFgnkCmUpQ"),
+        coreProgram: MPL_CORE_PROGRAM_ID,
+      })
+      .signers([anchor.Wallet.local().payer])
+      .rpc();
+
+    console.log(transferNftTx);
+  });
+  */
 });
